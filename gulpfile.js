4f6fb2b027192dd6a2331478731724513bd82479
Saif Tase <saiftase@gmail.com>
2016-05-26 13:11:56 -0400

    Caching main page, no templates

--- gulpfile.js
+++ gulpfile.js
@@ -98,6 +98,12 @@ gulp.task('buildCSS', function () {
         .pipe(gulp.dest('./public'));
 });
 
+gulp.task('copyHTML', function(){
+    return gulp.src(['./browser/js/**/*.html'], {base: './browser/'})
+    .pipe(gulp.dest('./public'))
+})
+
+
 // Production tasks
 // --------------------------------------------------------------
 
@@ -120,16 +126,43 @@ gulp.task('buildJSProduction', function () {
 
 gulp.task('buildProduction', ['buildCSSProduction', 'buildJSProduction']);
 
+// Service Worker Generation
+// --------------------------------------------------------------
+
+gulp.task('generateServiceWorker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+
+  var rootDir = 'public';
+
+
+  swPrecache.write(path.join(rootDir, 'service-worker.js'), {
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}'],
+    stripPrefix: rootDir,
+    dynamicUrlToDependencies: {
+        '/lodash/index.js': ['node_modules/lodash/index.js'],
+        '/angular/angular.js': ['node_modules/angular/angular.js'],
+        '/angular-animate/angular-animate.js': ['node_modules//angular-animate/angular-animate.js'],
+        '/angular-ui-router/release/angular-ui-router.js': ['node_modules/angular-ui-router/release/angular-ui-router.js'],
+        '/angular-ui-bootstrap/ui-bootstrap.js': ['node_modules/angular-ui-bootstrap/ui-bootstrap.js'],
+        '/angular-ui-bootstrap/ui-bootstrap-tpls.js': ['node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js'],
+        '/socket.io-client/socket.io.js': ['node_modules/socket.io-client/socket.io.js']
+    }
+  }, callback);
+});
+
+
 // Composed tasks
 // --------------------------------------------------------------
 
 gulp.task('build', function () {
     if (process.env.NODE_ENV === 'production') {
-        runSeq(['buildJSProduction', 'buildCSSProduction']);
+        runSeq(['buildJSProduction', 'buildCSSProduction', 'copyHTML', 'generateServiceWorker']);
     } else {
-        runSeq(['buildJS', 'buildCSS']);
+        runSeq(['buildJS', 'buildCSS', 'copyHTML', 'generateServiceWorker']);
     }
 });
+
 
 gulp.task('default', function () {
 
@@ -146,6 +179,9 @@ gulp.task('default', function () {
     });
 
     gulp.watch('server/**/*.js', ['lintJS']);
+
+    //Copy files to public when html changed
+    gulp.watch('browser/**/*.html', ['copyHTML']);
 
     // Reload when a template (.html) file changes.
     gulp.watch(['browser/**/*.html', 'server/app/views/*.html'], ['reload']);
---
+++ secrets.js
@@ -0,0 +1,4 @@
+module.exports = {
+	consumer_secret: 'potato',
+	pocket_consumer_key: '54713-04c3332fc56b78e8a8525d4e'
+}
