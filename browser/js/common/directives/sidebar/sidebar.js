df5fbed6346c7e958e2f58d9e5a735ee8a7bab5a
Jiacheng Yang <jy1964@nyu.edu>
2016-06-04 16:00:23 -0400

    finish sidebar framework

--- browser/js/common/directives/sidebar/sidebar.js
+++ browser/js/common/directives/sidebar/sidebar.js
@@ -3,5 +3,24 @@ app.directive('sidebar', function () {
 		restrict: 'E',
 		scope: {},
 		templateUrl: 'js/common/directives/sidebar/sidebar.html',
+		link: function(scope) {
+		    $(".menu-up").click(function(){
+		    	if($(this).css('transform')	!== 'none'){
+		    		$(this).css("transform", "");
+		    		if($(this).attr('id') === 'subscriptions-icon')
+		    			$('#subscriptions').show(400);
+		    		if($(this).attr('id') === 'folders-icon')
+		    			$('#folders').show(400);
+		    	}
+		    	else{
+					$(this).css("transform", "rotate(180deg)");
+		    		if($(this).attr('id') === 'subscriptions-icon')
+		    			$('#subscriptions').hide(400);
+		    		if($(this).attr('id') === 'folders-icon')
+		    			$('#folders').hide(400);				
+		    	}
+			});
+
+		}
 	}
 })
