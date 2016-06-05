cafbaf70b9831c6f01926b6436a0d5bc631a5a76
John Nieves <karn09@gmail.com>
2016-06-05 10:49:20 -0400

    ng-repeating over menu items

--- browser/js/common/directives/speed-dial/speed-dial.js
+++ browser/js/common/directives/speed-dial/speed-dial.js
@@ -1,13 +1,21 @@
-app.directive('speedDial', function() {
-  return {
-    restrict: 'E',
-    scope: {},
-    templateUrl: 'html/speed-dial/speed-dial.html',
-    link: function (scope, element, attribute) {
-      scope.isOpen = false;
-      scope.count = 0;
-      scope.hidden = false;
-      scope.hover = false;
-    }
-  }
+app.directive('speedDial', function () {
+	return {
+		restrict: 'E',
+		scope: {},
+		templateUrl: 'html/speed-dial/speed-dial.html',
+		link: function (scope, element, attribute) {
+			scope.isOpen = false;
+			scope.count = 0;
+			scope.hidden = false;
+			scope.hover = false;
+      scope.items = [{
+				name: "Add URL",
+				icon: "/icons/ic_add_white_36px.svg"
+			}, {
+				name: "Add Category",
+				icon: "/icons/ic_playlist_add_white_36px.svg"
+			}];
+
+		}
+	}
 })
