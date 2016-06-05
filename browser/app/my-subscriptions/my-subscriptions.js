21b4b53d75357a64944c118a5c3703c165006266
Jiacheng Yang <jy1964@nyu.edu>
2016-06-05 18:01:38 -0400

    finish my-subscriptions-page framework

--- browser/app/my-subscriptions/my-subscriptions.js
+++ browser/app/my-subscriptions/my-subscriptions.js
@@ -1,6 +1,23 @@
 app.config(function ($stateProvider) {
 	$stateProvider.state('mySubscriptions', {
 		url: '/subscriptions',
-		templateUrl: 'app/my-subscriptions/my-subscriptions.html'
+		templateUrl: 'app/my-subscriptions/my-subscriptions.html',
+		controller: 'mySubscriptionsCtrl'
 	});
+});
+
+app.controller('mySubscriptionsCtrl', function($scope) {
+
+	$(".menu-up").click(function(){
+		var id = '#' + $(this).attr('id').slice(0,-8);
+		if($(this).css('transform')	!== 'none'){
+			$(this).css("transform", "");
+			$(id).show(400);
+		}
+		else{
+			$(this).css("transform", "rotate(180deg)");
+			$(id).hide(400);				
+		}
+	});
+
 });
