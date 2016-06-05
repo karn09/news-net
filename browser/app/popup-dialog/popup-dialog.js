4da366ad1d20883017bfac1f90c91d980af8efeb
John Nieves <karn09@gmail.com>
2016-06-05 15:58:02 -0400

    changes to add article- not working when not logged in

--- browser/app/popup-dialog/popup-dialog.js
+++ browser/app/popup-dialog/popup-dialog.js
@@ -1,10 +1,19 @@
-app.controller('dialogFormCtrl', function($mdDialog) {
-  this.close = function() {
-    $mdDialog.cancel();
-  };
-  this.submit = function(type, data) {
-    // if type category, send to category api
-    // if type url, send to url api
-    $mdDialog.hide();
-  }
+app.controller('dialogFormCtrl', function ($mdDialog, ParserFactory, Session) {
+	this.close = function () {
+		$mdDialog.cancel();
+	};
+	this.submit = function (type, data) {
+		// if type category, send to category api
+		// if type url, send to url api
+		if (type === 'url') {
+			ParserFactory.parseUrl(data, Session.user._id)
+				.then(function (response) {
+					$mdDialog.hide();
+					$scope.parsed = response;
+				});
+		} else if (type === 'category') {
+      // not set up yet
+			$mdDialog.hide();
+		}
+	}
 })
