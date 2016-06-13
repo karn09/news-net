f8fb49674ada1c8bb98a7c6e3072fac2c26ce863
John Nieves <karn09@gmail.com>
2016-06-13 08:05:18 -0400

    changes to sw cache

--- browser/app/common/factories/articleDetail.js
+++ browser/app/common/factories/articleDetail.js
@@ -25,8 +25,17 @@ app.factory('ArticlesFactory', function($http, idbService) {
   detailObj.fetchCategories = function(){
     return $http.get("/api/categories/")
     .then(function(response){
-      console.log(response.data)
-      return response.data;
+      return idbService.add('categories', response.data)
+        .then(function(data) {
+          console.log(data)
+          console.log('inner ', response.data)
+          return response.data;
+        })
+        .catch(function(err) {
+          console.log(err);
+          console.log(response.data)
+          return response.data
+        })
     })
   }
 
--- browser/app/idb/idb.js
+++ browser/app/idb/idb.js
@@ -1,33 +1,53 @@
 (function () {
-  // 'use strict';
+		// 'use strict';
 
-  if (!window.angular) throw new Error('Angular required');
-  var idb = angular.module('dexieIdb', []);
+		if (!window.angular) throw new Error('Angular required');
+		var idb = angular.module('dexieIdb', []);
 
-  idb.factory('idbService', function ($log) {
-    console.log('idbService loading..');
-    if (!window.Dexie) throw new Error('Dexie not found');
-    var db = new Dexie('newsDb');
-    db.version(1).stores({
-      categories: "++id, description, type, *pages",
-      page: "++id, content, datePublished, domain, excerpt, title, url, __v, leadImageUrl"
-    });
+		idb.factory('idbService', function ($log, $q) {
+			console.log('idbService loading..');
+			if (!window.Dexie) throw new Error('Dexie not found');
+			var db = new Dexie('newsDb');
 
-    db.open().then(function () {
-      db.close();
-      db.open().then(function () {
-        $log.debug('Opening connection to indexedDb');
-      });
-    });
+      db.version(1).stores({
+				categories: "&_id, description, type, *pages",
+				page: "++_id, _id, content, datePublished, domain, excerpt, title, url, __v, leadImageUrl"
+			});
 
-    db.on('blocked', function (err) {
-      $log.warn('blocked ', err);
-    });
+			db.open().then(function () {
+				db.close();
+				db.open().then(function () {
+					$log.debug('Opening connection to indexedDb');
+				});
+			});
 
-    return {
-      getAll: function getAll() {
-        return '123';
-      }
-    };
-  });
+			db.on('blocked', function (err) {
+				$log.warn('blocked ', err);
+			});
+
+			return {
+				getAll: function () {
+					return '123';
+				},
+				add: function (store, value, id) {
+					var deferred = $q.defer();
+					if (store === 'categories') {
+						db.categories.bulkAdd(value)
+							.then(function (data) {
+								deferred.resolve(data);
+							})
+              .catch(function(err) {
+                deferred.resolve(err);
+              })
+					} else if (store === 'page') {
+						db.page.add(value)
+							.then(function (data) {
+								deferred.resolve(data);
+							})
+					}
+					return deferred.promise;
+				}
+			};
+		});
+
 })();
