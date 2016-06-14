a6d41367a3a40543d86deef1fda0bc42760cdf55
Saif Tase <saiftase@gmail.com>
2016-06-14 01:40:44 -0400

    Buttons showing conditionally

--- browser/app/common/directives/speed-dial/speed-dial.js
+++ browser/app/common/directives/speed-dial/speed-dial.js
@@ -14,41 +14,90 @@ app.directive('speedDial', function ($mdDialog, $state, $rootScope) {
 
 			scope.$root.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
 				console.log("Link State: ", toState.name);
-			});
-			
 
-			scope.isOpen = false;
-			scope.count = 0;
-			scope.hidden = false;
-			scope.hover = false;
-			scope.items = [{
-				name: "Add URL",
-				icon: "/assets/icons/ic_add_white_36px.svg",
-				type: "url",
-				direction: "top"
-			}, {
-				name: "Add Category",
-				type: "category",
-				icon: "/assets/icons/ic_playlist_add_white_36px.svg",
-				direction: "bottom"
-			}];
+				var optionsByState = {
+					default: {
+						isOpen: false,
+						count: 0,
+						hidden: false,
+						hover: false,
+						items: [
+							{
+								name: "Add URL",
+								icon: "/assets/icons/ic_add_white_36px.svg",
+								type: "url",
+								direction: "top"
+							},
+							{
+								name: "Add Category",
+								type: "category",
+								icon: "/assets/icons/ic_playlist_add_white_36px.svg",
+								direction: "bottom"
+							}
+						],
+						takeAction: openDialog
+					},
 
+					article: {
+						isOpen: false,
+						count: 0,
+						hidden: false,
+						hover: false,
+						items: [
+							{
+								name: "Jump to discussion",
+								type: "discussion",
+								icon: "/assets/icons/ic_chat_48px.svg",
+								direction: "top"
+							},
+							{
+								name: "Placeholder",
+								type: "placeholder",
+								icon: "",
+								direction: "bottom"
+							}
 
-			scope.openDialog = function ($event, item) {
-				$mdDialog.show({
-					scope: this,
-					preserveScope: true,
-					clickOutsideToClose: true,
-					controller: 'dialogFormCtrl',
-					controllerAs: 'dialog',
-					templateUrl: '/app/popup-dialog/popup-dialog.html',
-					targetEvent: $event,
-					locals: {
-						item: item
+						],
+						takeAction: jumpTo
 					}
-				})
-			}
+				} //End optionsByState
 
-		}
+				function openDialog($event, item) {
+
+					console.log("Open Dialog");
+
+					$mdDialog.show({
+						scope: this,
+						preserveScope: true,
+						clickOutsideToClose: true,
+						controller: 'dialogFormCtrl',
+						controllerAs: 'dialog',
+						templateUrl: '/app/popup-dialog/popup-dialog.html',
+						targetEvent: $event,
+						locals: {
+							item: item
+						}
+					})
+				}
+
+				function jumpTo($event, item){
+
+					console.log("Jump to state.");
+
+					$state.go('home')
+				}
+
+				if(optionsByState[toState.name]){
+					for(var key in optionsByState[toState.name]){
+						scope[key] = optionsByState[toState.name][key];
+					}
+				}else{
+					for(var key in optionsByState["default"]){
+						scope[key] = optionsByState["default"][key];
+					}
+				}
+
+			}); //End $on stateChangeSuccess
+		} //End link
 	}
 })
