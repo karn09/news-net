687655ebdfbfd181ab034244d433e158d08a7a43
Saif Tase <saiftase@gmail.com>
2016-06-19 16:12:37 -0400

    Set rules for folders/subscriptions, added card icons

--- server/db/models/category.js
+++ server/db/models/category.js
@@ -1,20 +1,46 @@
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['public', 'private']
+    },
     description: {
          type: String
     },
     pages: [{
         type: mongoose.Schema.Types.ObjectId, ref: 'Page'
     }],
-    type: {
-        type: String,
-        enum: ['public', 'private']
-    },
     admin: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User'
+    },
+    subscribers: {
         type: mongoose.Schema.Types.ObjectId, ref: 'User'
     }
 });
 
+schema.pre('save', function (next) {
+
+    //To-Do: Name changes only allowed for folders - DELETE en route
+    
+    if(this.isNew){
+        //Automatically subscribe admin to list.
+        this.subscribers.push(this.admin);
+
+        //If category is public (i.e. 'Subscription'), make sure name is unique. 
+        if(this.type === 'public'){
+           Category.count({type: 'public', description: this.description})
+           .then(function(count){
+               if(count > 0) next(new ValidationError('Subscription with name ${this.type} already exists.'))
+           })
+        }
+    }else{
+        if(this.type === 'private' && this.subscribers.length > 1){
+            next(new ValidationError('Users cannot subscribe to private folders.'));
+        }
+    }
+    
+    next();
+});
 
 mongoose.model('Category', schema);
