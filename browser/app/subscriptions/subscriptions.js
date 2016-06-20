8bc66f607d6facddb82bdfd9faae90a98cc08bb2
Saif Tase <saiftase@gmail.com>
2016-06-20 12:23:31 -0400

    Begin work on subscription/folder child states

--- browser/app/subscriptions/subscriptions.js
+++ browser/app/subscriptions/subscriptions.js
@@ -1,18 +1,33 @@
 app.config(function ($stateProvider) {
-	$stateProvider.state('subscriptions', {
-		url: '/subscriptions',
-		templateUrl: 'app/subscriptions/subscriptions.html',
-		resolve: {
-			categories: function(CategoriesFactory){
-				return CategoriesFactory.getUserSubscriptionsDetailed();
+	$stateProvider
+		.state('subscriptions', {
+			url: '/subscriptions',
+			templateUrl: 'app/subscriptions/subscriptions.html',
+			resolve: {
+				categories: function(CategoriesFactory){
+					return CategoriesFactory.getUserSubscriptionsDetailed();
+				}
+			},
+			controller: 'SubscriptionsCtrl'
+		})
+
+		.state('subscriptions.section', {
+			parent: 'subscriptions',
+			url: '/:name',
+			templateUrl: 'app/subscriptions/subscriptions.html',
+			controller: function(){
+				console.log("hfhrf")
 			}
-		},
-		controller: 'SubscriptionsCtrl'
-	});
+		})
 });
+
 
 app.controller('SubscriptionsCtrl', function($scope, categories) {
 
 	$scope.categories = categories;
 
+});
+
+app.controller('SubscriptionSectionCtrl', function($scope, $stateParams, categories){
+	console.log("HI FROM CTRL")
 });
