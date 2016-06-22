180e2a749fefab3e177d739db008fd309b9f9381
Jiacheng Yang <jy1964@nyu.edu>
2016-06-21 22:00:34 -0400

    reload page when log out, realize hompage toggle

--- browser/app/home/home.js
+++ browser/app/home/home.js
@@ -1,6 +1,17 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'app/home/home.html'
+        templateUrl: 'app/home/home.html',
+        resolve: {
+        	user: function(AuthService) {
+        		return AuthService.getLoggedInUser();
+        	}
+        },
+        controller: 'HomepageCtrl'
     });
+});
+
+app.controller('HomepageCtrl', function($scope, user) {
+	$scope.user = user;
+	console.log($scope.user);
 });
