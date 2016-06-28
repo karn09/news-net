4d25dfeed7926e8b2c2cabc811379b841e6eed72
John Nieves <karn09@gmail.com>
2016-06-28 02:53:53 -0400

    leaving it alone before i break things

--- browser/app/home/home.js
+++ browser/app/home/home.js
@@ -1,50 +1,67 @@
-app.config(function ($stateProvider) {
-    $stateProvider.state('home', {
-        url: '/',
-        templateUrl: 'app/home/home.html',
-        resolve: {
-        	user: function(AuthService) {
-        		return AuthService.getLoggedInUser();
-        	},
-            articles: function(ArticlesFactory){
-                return ArticlesFactory.fetchRecommendedArticles();
-            }            
-        },
-        controller: 'HomepageCtrl'
-    });
+app.config(function ($stateProvider, $urlRouterProvider) {
+	$stateProvider.state('home', {
+		url: '/home',
+		templateUrl: 'app/home/home.html',
+		resolve: {
+			user: function (AuthService) {
+				return AuthService.getLoggedInUser();
+			},
+			articles: function (ArticlesFactory) {
+				return ArticlesFactory.fetchRecommendedArticles();
+			},
+      allArticles: function(ArticlesFactory) {
+        return ArticlesFactory.fetchAll();
+      }
+		},
+		controller: 'HomepageCtrl'
+	});
+	$urlRouterProvider.when('/', '/home');
+
 });
 
-app.controller('HomepageCtrl', function($scope, $rootScope, user, articles, $state) {
+app.controller('HomepageCtrl', function ($scope, $rootScope, user, articles, $state) {
 	$scope.user = user;
-    $scope.articles = articles;
+	$scope.articles = articles;
 
-    $scope.listArticles = function(id){
-        $state.go('article', {id: id});
-    };
+	$scope.listArticles = function (id) {
+		$state.go('article', {
+			id: id
+		});
+	};
 
 
-    $scope.tiles = buildGridModel({
-            id   : "",
-            title: "",
-            image: ""
-          });
-    function buildGridModel(tileTmpl){
-      var it, results = [ ];
-      for (var j=0; j<11; j++) {
-        var article = articles[Math.floor(articles.length * Math.random())];
-        it = angular.extend({},tileTmpl);
-        it._id = article._id;
-        it.title = article.title;
-        it.span  = { row : 1, col : 1 };
-        it.image = article.leadImageUrl;
-        switch(j+1) {
-          case 1: it.span.row = it.span.col = 2;   break;
-          case 4: it.span.col = 2;                 break;
-          case 5: it.span.row = it.span.col = 2;   break;
-        }
-        results.push(it);
-      }
-      return results;
-    }
+	$scope.tiles = buildGridModel({
+		id: "",
+		title: "",
+		image: ""
+	});
+
+	function buildGridModel(tileTmpl) {
+		var it, results = [];
+		for (var j = 0; j < 11; j++) {
+			var article = articles[Math.floor(articles.length * Math.random())];
+			it = angular.extend({}, tileTmpl);
+			it._id = article._id;
+			it.title = article.title;
+			it.span = {
+				row: 1,
+				col: 1
+			};
+			it.image = article.leadImageUrl;
+			switch (j + 1) {
+			case 1:
+				it.span.row = it.span.col = 2;
+				break;
+			case 4:
+				it.span.col = 2;
+				break;
+			case 5:
+				it.span.row = it.span.col = 2;
+				break;
+			}
+			results.push(it);
+		}
+		return results;
+	}
 
 });
