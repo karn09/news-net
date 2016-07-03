9bb29e8bb3b55534a7b0a4b594b66e03ad705bbb
John Nieves <karn09@gmail.com>
2016-07-03 17:48:31 -0400

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -358,7 +358,8 @@ gulp.task('generateServiceWorker', function (callback) {
 		stripPrefix: rootDir,
 		dynamicUrlToDependencies: dependencies,
 		runtimeCaching: runtimeCachingOptions,
-		verbose: true
+		verbose: true,
+		handleFetch: false,
 	}, callback);
 });
 
@@ -394,13 +395,13 @@ gulp.task('buildProduction', ['buildCSSProduction', 'buildJSProduction', 'buildI
 
 // gulp.task('build', function () {
 //     runSeq(['buildJS', 'buildCSS', 'copyImages', 'buildIDB', 'copyFonts', 'copyHTML', 'generateServiceWorker', 'copyExtensionAssets']);
-    
+
 //     if (process.env.NODE_ENV === 'production') {
 //         runSeq(['buildJSProduction', 'buildCSSProduction', 'buildIDB', 'copyImages', 'copyFonts', 'copyHTML', 'generateServiceWorker', 'copyExtensionAssets']);
 //     } else {
 //         runSeq(['buildJS', 'buildCSS', 'copyImages', 'buildIDB', 'copyFonts', 'copyHTML', 'generateServiceWorker', 'copyExtensionAssets']);
 //     }
-    
+
 // });
 
 
