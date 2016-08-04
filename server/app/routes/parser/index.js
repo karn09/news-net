840e367a29c7b6826243d2ad07b69e7efa56cc95
Saif Tase <saiftase@gmail.com>
2016-08-04 16:37:15 -0400

    Regenerate keys, obfuscate secrets

--- server/app/routes/parser/index.js
+++ server/app/routes/parser/index.js
@@ -1,25 +1,19 @@
 'use strict';
 var router = require('express').Router();
+var secrets = require('../../../../secrets.js')
 module.exports = router;
 
 var readability = require('readability-api');
-
-readability.configure({
-    consumer_key: 'ontima',
-    consumer_secret: 'D3d3qjF22SpWTzGLQAjJs9VeQtfFFVFU',
-    parser_token: 'a39d4eda19236cb88d0ac6cb68c579a3d04c65c4'
-});
+readability.configure(secrets.readability);
 
 // Create a parser object
 var parser = new readability.parser();
 
 
 router.get('/:url', function(req, res, next){
-	//console.log("url: ", req.params.url);
 	parser.parse(req.params.url, function (err, parsed) {
 	  res.json(parsed);
 	});
-
 });
 
 
--- server/env/development.js
+++ server/env/development.js
@@ -1,6 +1,8 @@
+var secrets = require('../../secrets.js')
+
 module.exports = {
-  "DATABASE_URI": "mongodb://localhost:27017/news-net",
-  "SESSION_SECRET": "Optimus Prime is my real dad",
+  "DATABASE_URI": secrets.databaseURI,
+  "SESSION_SECRET": secrets.sessionSecret,
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
     "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
@@ -11,9 +13,5 @@ module.exports = {
     "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
     "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
   },
-  "GOOGLE": {
-    "clientID": "385464584294-gfskje0bo5ajq6mtjnerc31ne7lf50t6.apps.googleusercontent.com",
-    "clientSecret": "6wBmntvRp6U566Tb0t3irtyY",
-    "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
-  }
+  "GOOGLE": secrets.google
 };
